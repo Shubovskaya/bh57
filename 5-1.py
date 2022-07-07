c5a3494e2240f061761fd34bec1c1da7d3d9c65c
Shubovskaya <chibalice@yandex.ru>
2022-07-07 15:26:03 +0300

    add task 5-1

--- 5-1.py
+++ 5-1.py
@@ -1,1 +1,12 @@
 # Вывести первые N чисел кратные M и больше K
+
+N: int = int(input("Введите число: "))
+M: int = int(input("Введите число: "))
+K: int = int(input("Введите число: "))
+
+for i in range(0, N, M):
+    if i > K:
+        print(i)
+
+
+

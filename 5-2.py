6872e525075dad2db14067e464c08d9032f7ae66
Shubovskaya <chibalice@yandex.ru>
2022-07-07 14:05:43 +0300

    add task 5-2

---
+++ 5-2.py
@@ -0,0 +1,19 @@
+# Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число
+
+a = float(input("Введите первое число: "))
+operation = input("Введите действие: (+,-,*,/: ")
+b = float(input("Введите второе число: "))
+result = 0
+
+if operation == "+":
+    result = a+b
+elif operation == "-":
+    result = a-b
+elif operation == "*":
+    result = a*b
+elif operation == "/":
+    result = a/b
+else:
+    result = None
+print(f"Результат: {result}")
+

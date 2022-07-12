5c9bf35b06a21500c2a1ca4dd3fe809a3d6a62fa
Shubovskaya <chibalice@yandex.ru>
2022-07-12 12:12:06 +0300

    add task 6-2

--- 6-1.py
+++ 6-1.py
@@ -1,13 +1,22 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без
 # использования функции int
 
-def number(n):
-    if n
-    s = ' '
-     while n > 0:
-         s = str(n % 2) + s
-       n //= 2
+def binary(n):
+    s = ''
+    while n > 0:
+        s = str(n % 2) + s
+        n //= 2
     return s
+n = 10
+print(str(binary))
+
+# while 1:
+#     n = int(input())
+#     if n != 0:
+#         print(binary(n))
+#     else:
+#         break
+
 
 
 

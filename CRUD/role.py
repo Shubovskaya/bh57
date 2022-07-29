08f6824f85596d2e47eb006dddf8d6b5b54fa648
Shubovskaya <chibalice@yandex.ru>
2022-07-29 13:10:28 +0300

    add commit

---
+++ CRUD/role.py
@@ -0,0 +1,14 @@
+from engine import create_session
+
+class CRUDRole:
+
+    @staticmethod
+    @create_session
+    def add(name: str, cur=None, conn=None) -> None:
+        cur.execute("""
+            INSERT INTO categories(name)
+            VALUES(?);
+        """, (name, ))
+        conn.commit()
+
+

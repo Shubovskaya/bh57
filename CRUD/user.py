a3b27c25920d3848f20a88f633513aa8f3a53ede
Shubovskaya <chibalice@yandex.ru>
2022-07-29 13:08:03 +0300

    add commit

---
+++ CRUD/user.py
@@ -0,0 +1,19 @@
+from engine import create_session
+
+class CRUDUser:
+
+    @staticmethod
+    @create_session
+    def add(
+            login: int,
+            password: str,
+            email: str,
+            role_id: int,
+            cur=None,
+            conn=None
+    ) -> None:
+        cur.execute("""
+            INSERT INTO users(login, password, email, role_id) 
+            VALUES(?, ?, ?, ?);
+        """, (login, password, email, role_id))
+        conn.commit()

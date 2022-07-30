b36f14eccb4ab7bdaa461e2a279b0d659b47a231
Shubovskaya <chibalice@yandex.ru>
2022-07-30 15:25:06 +0300

    add commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from CRUD import CRUDArticle
+# from CRUD import CRUDArticle
+#
+# print(CRUDArticle.add(title="Автомобили", body="марка", category_id=1, users_id=1))
 
-print(CRUDArticle.add(title="Автомобили", body="марка", category_id=1, users_id=1))
 
-
-print()
+print("Hello, world")
